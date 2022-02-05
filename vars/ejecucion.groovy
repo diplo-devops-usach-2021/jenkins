642414d9f38a5be9b55b1179cd2fc338968533e4
psantacruzp <95259906+psantacruzp@users.noreply.github.com>
2022-02-05 17:31:51 -0300

    Ajuste formato y correccion de erorres

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -1,26 +1,25 @@
 def call() {
     pipeline {
-            agent any
+        agent any
 
-            environment {
-			    STAGE = ''
-		    }
+        environment {
+		    STAGE = ''
+		}
 
-            parameters {
-			    choice choices: ['gradle', 'maven'], description: 'Indicar la herramienta de construccion.', name: 'buildTool'
-			    string(name: 'Stage', defaultValue: '', description: 'Ingresar los Stages de ejecucion')
-		    }
+        parameters {
+		    choice choices: ['gradle', 'maven'], description: 'Indicar la herramienta de construccion.', name: 'buildTool'
+		    string(name: 'Stage', defaultValue: '', description: 'Ingresar los Stages de ejecucion')
+		}
 
-            options {
-                timestamps()
-            }
+        options {
+            timestamps()
+        }
 
-            stages {
-                stage('Pipeline') {
-                    steps {
-                        script {
-                            println 'Pipeline'
-                            
+        stages {
+            stage('Pipeline') {
+                steps {
+                    script {
+                        println 'Pipeline'
                             def ci_or_cd = verifyBranchName()
                             
                             if (params.buildTool == 'gradle'){
@@ -28,29 +27,29 @@ def call() {
                             } else {
                                 def ejecucion = load 'maven.groovy'
                                 maven(verifyBranchName())
-                                }
-                            }
                         }
                     }
-		    }
-            post {
-                always {
-                    slackSend channel: '#jenkins-ci', color: 'normal', message: "${username}" + ' ha ejecutado un Pipeline.', teamDomain: 'dipdevopsusac-tr94431', tokenCredentialId: 'slack-token'
-                    slackSend channel: '#jenkins-ci', color: 'normal', message: 'Job Name: ' + env.JOB_NAME + ', BuildTool: ' +  params.buildTool + '.', teamDomain: 'dipdevopsusac-tr94431', tokenCredentialId: 'slack-token'
-                }
-                success{
-                    slackSend channel: '#jenkins-ci', color: '#29AE4A', message: 'Ejecucion exitosa de ' + verifyBranchName() + '. Se han ejecutado los siguientes Stages: ' + params.Stage + '.', teamDomain: 'dipdevopsusac-tr94431', tokenCredentialId: 'slack-token'
-                }
-                failure {
-                    slackSend channel: '#jenkins-ci', color: '#EC4D34', message: 'Ejecucion Fallida en Stage: ' + "${STAGE}" + '.', teamDomain: 'dipdevopsusac-tr94431', tokenCredentialId: 'slack-token'
                 }
             }
+		}
+
+        post {
+            always {
+                slackSend channel: '#jenkins-ci', color: 'normal', message: "${username}" + ' ha ejecutado un Pipeline.', teamDomain: 'dipdevopsusac-tr94431', tokenCredentialId: 'slack-token'
+                slackSend channel: '#jenkins-ci', color: 'normal', message: 'Job Name: ' + env.JOB_NAME + ', BuildTool: ' +  params.buildTool + '.', teamDomain: 'dipdevopsusac-tr94431', tokenCredentialId: 'slack-token'
+            }
+            success{
+                slackSend channel: '#jenkins-ci', color: '#29AE4A', message: 'Ejecucion exitosa de ' + verifyBranchName() + '. Se han ejecutado los siguientes Stages: ' + params.Stage + '.', teamDomain: 'dipdevopsusac-tr94431', tokenCredentialId: 'slack-token'
+            }
+            failure {
+                slackSend channel: '#jenkins-ci', color: '#EC4D34', message: 'Ejecucion Fallida en Stage: ' + "${STAGE}" + '.', teamDomain: 'dipdevopsusac-tr94431', tokenCredentialId: 'slack-token'
+            }
+        }
 	}
 
 }
 
 def verifyBranchName(){
-	//def CiOrCd = (env.GIT_BRANCH.contains('feature-') ) ? 'CI' : 'CD'
 	if (env.GIT_BRANCH.contains('feature-') || env.GIT_BRANCH.contains('develop')){
 		return 'CI'
 	} else {
