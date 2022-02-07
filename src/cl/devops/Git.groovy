a86f8aa56d21ac0ab8a63ced3ff3740baa8d814f
Jorge <jgarciam@users.noreply.github.com>
2022-02-06 23:46:02 -0300

    fix

--- src/cl/devops/Git.groovy
+++ src/cl/devops/Git.groovy
@@ -4,14 +4,15 @@ package cl.devops
 def merge(String ramaOrigen, String ramaDestino){
     println "Realizando merge desde ${ramaOrigen} a ${ramaDestino}"
     sh """
-        git checkout -f ${ramaDestino}
+        git checkout ${ramaDestino}
         git merge ${ramaOrigen}
     """
 }
 
 def tag(String version, String descripcion){
     println "Realizando Tag: ${version} descripcion:  ${descripcion}"
-    sh(returnStdout:false , script: "git pull origin main")
-    sh(returnStdout:false , script: "git checkout ${ramaDestino}")
-    sh(returnStdout:false , script: "git tag -a ${version} -m '${descripcion}'")
+    sh """
+        git checkout ${ramaDestino}
+        git tag -a ${version} -m '${descripcion}'
+    """
 }
