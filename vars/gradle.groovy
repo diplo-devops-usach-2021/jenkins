2a73777ce0329b460a80760f21e1be79e58fea02
Jorge <jgarciam@users.noreply.github.com>
2022-02-06 21:16:19 -0300

    test

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -98,4 +98,12 @@ def call(String pipelineType){
 	}
 }
 
+String obtieneRamaActual(){
+    String ramaActual = sh(
+				script: "git rev-parse --abbrev-ref HEAD",
+				returnStdout: true
+            )
+    return ramaActual
+}
+
 return this;
