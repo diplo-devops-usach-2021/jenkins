a6f46798ee233cdb947092b1591d111f63e346ef
Jorge <jgarciam@users.noreply.github.com>
2022-02-07 11:20:05 -0300

    fix

--- src/cl/devops/Git.groovy
+++ src/cl/devops/Git.groovy
@@ -3,19 +3,23 @@ package cl.devops
 
 def merge(String ramaOrigen, String ramaDestino){
     println "Realizando merge desde ${ramaOrigen} a ${ramaDestino}"
-    git branch: "${ramaDestino}", credentialsId: 'github-user', url: 'https://github.com/diplo-devops-usach-2021/ms-iclab.git'
-    sh """
-        git config --global user.email "jgarciam@gmail.com"
-        git config --global user.name "Jorge Garcia"
-        git merge origin/${ramaOrigen}
-    """
+    //git branch: "${ramaDestino}", credentialsId: 'github-user', url: 'https://github.com/diplo-devops-usach-2021/ms-iclab.git'
+    withCredentials([usernamePassword(credentialsId: 'github-user', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
+        sh """          
+            git checkout ${ramaDestino}
+            git merge origin/${ramaOrigen}
+        """
+    }
 }
-
+//git config --global user.email "jgarciam@gmail.com"
+//git config --global user.name "Jorge Garcia"
 def tag(String version, String descripcion){
     println "Realizando Tag: ${version} descripcion:  ${descripcion}"
     git branch: "main", credentialsId: 'github-user', url: 'https://github.com/diplo-devops-usach-2021/ms-iclab.git'    
     withCredentials([usernamePassword(credentialsId: 'github-user', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
-        sh "git tag -a \'${version}\' -m \'${descripcion}\'"
-        sh('git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/diplodevops/ms-iclab.git')
+        sh """
+            git tag -a \'${version}\' -m \'${descripcion}\'
+            git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/diplo-devops-usach-2021/ms-iclab.git
+        """
     }
 }
