e12542e1399f268d1e0b0b618a9cb9bcca2701e3
Jorge <jgarciam@users.noreply.github.com>
2022-02-07 17:55:11 -0300

    fww

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -56,7 +56,7 @@ def call(String pipelineType){
 		if(params.Stage.contains('nexusDownload')){
 			stage("nexusDownload"){
 				def groupId = pom.groupId.replace(".","/")
-				sh "curl -X GET -u admin:yakarta123. http://nexus:8081/repository/test-nexus/${groupId}/${pom.artifactId}/${pom.version}/${pom.artifactId}-${pom.version}.jar -O"
+				sh "curl -X GET -u admin:yakarta123. http://nexus:8081/repository/test-nexus/${groupId}/${pom.artifactId}/${pom.version}/${pom.artifactId}-1.0.0.jar -O"
 			}
 		}
 		if(params.Stage.contains('run')){
