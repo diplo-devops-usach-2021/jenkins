88b4bf9541997a7ebecf43bbdd9a44417c6ae85a
Jorge <jgarciam@users.noreply.github.com>
2022-02-07 10:16:42 -0300

    test

--- src/cl/devops/Git.groovy
+++ src/cl/devops/Git.groovy
@@ -16,6 +16,6 @@ def tag(String version, String descripcion){
     git branch: "main", credentialsId: 'github-user', url: 'https://github.com/diplo-devops-usach-2021/ms-iclab.git'
     sh "git tag -a \'${version}\' -m \'${descripcion}\'"
     withCredentials([usernamePassword(credentialsId: 'github-user', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
-                        sh('git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com:diplodevops/ms-iclab.git')
+                        sh('git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/diplodevops/ms-iclab.git')
                     }
 }
