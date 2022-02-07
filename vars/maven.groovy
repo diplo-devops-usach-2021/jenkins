2e3fc9ce62c74aa4915944c22beadc9f6022faaf
Jorge <jgarciam@users.noreply.github.com>
2022-02-07 17:59:36 -0300

    sfsfs

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -63,8 +63,8 @@ def call(String pipelineType){
 		if(params.Stage.contains('run')){
 			stage("run"){
 				sh "chmod +X ${pom.artifactId}-1.0.0.jar"
-				sh "nohup java -jar ${pom.artifactId}-1.0.0.jar"
-				sleep(20)
+				sh "nohup java -jar ${pom.artifactId}-1.0.0.jar &"
+				sleep(10)
 			}
 		}
 		if(params.Stage.contains('test')){
