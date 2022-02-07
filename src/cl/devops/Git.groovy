f7060f788d134385b65de63e1c1021443f1788bf
Jorge <jgarciam@users.noreply.github.com>
2022-02-07 00:09:57 -0300

    fix errors

--- src/cl/devops/Git.groovy
+++ src/cl/devops/Git.groovy
@@ -14,7 +14,7 @@ def merge(String ramaOrigen, String ramaDestino){
 def tag(String version, String descripcion){
     println "Realizando Tag: ${version} descripcion:  ${descripcion}"
     sh """
-        git checkout ${ramaDestino}
-        git tag -a ${version} -m '${descripcion}'
+        git branch: "${main}", credentialsId: 'github-user', url: 'https://github.com/diplo-devops-usach-2021/ms-iclab.git'
+        git tag -a ${version} -m "${descripcion}"
     """
 }
