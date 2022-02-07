001c09e7b7b0f24b28976d9e00a24c616d561814
Jorge <jgarciam@users.noreply.github.com>
2022-02-07 00:36:50 -0300

    sdfsfs

--- src/cl/devops/Git.groovy
+++ src/cl/devops/Git.groovy
@@ -15,10 +15,7 @@ def tag(String version, String descripcion){
     println "Realizando Tag: ${version} descripcion:  ${descripcion}"
     git branch: "main", credentialsId: 'github-user', url: 'https://github.com/diplo-devops-usach-2021/ms-iclab.git'
     sh "git tag -a \'${version}\' -m \'${descripcion}\'"
-    
-    sh """
-        git config --global user.email "jgarciam@gmail.com"
-        git config --global user.name "Jorge Garcia"
-        git push origin main
-    """
+    withCredentials([usernamePassword(credentialsId: 'github-user', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
+                        sh('git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com:diplodevops/ms-iclab.git')
+                    }
 }
